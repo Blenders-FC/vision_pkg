ba01a29e7128b6263a3e3f8ec8451f6357afd1b8
soph-lego <a01639462@tec.mx>
2025-06-18 12:01:07 -0600

    red lower put toghether, same with red upper

--- src/detectarEquipo.py
+++ src/detectarEquipo.py
@@ -11,9 +11,9 @@ lower_red1 = np.array([0, 100, 150], np.unit8)
 upper_red1 = np.array([10, 255, 255], np.unit8) 
 lower_red2 = np.array([170, 100, 100], np.unit8) 
 upper_red2 = np.array([180, 255, 255], np.unit8) 
-Red1 = [lower_red1, upper_red1]
-Red2 = [lower_red2, upper_red2]
-Rmask = [Red1, Red2]
+Red_lower = [lower_red1, lower_red2]
+Red_upper = [upper_red1_red2, upper_red2]
+Rmask = [Red_lower, Red_upper]
 
 azulHigh = np.array([150, 220, 255], np.uint8)
 azulLow = np.array([90, 0, 120], np.uint8)
