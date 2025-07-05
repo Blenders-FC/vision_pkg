555dd2f485f6b606680816eb8b8ec562dfaa062b
CesarSimental-A01641385 <cesarsimentald@gmail.com>
2025-07-05 15:19:51 -0600

    add engine rhoban v1(only ball)

--- src/export.py
+++ src/export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a YOLO11n PyTorch model
-model = YOLO("best-nv2-62.pt")
+model = YOLO("best-rhoban-v1.pt")
 
 # Export the model to TensorRT
 model.export(format="engine", half=True)  # dla:0 or dla:1 corresponds to the DLA cores
--- src/test_detection.py
+++ src/test_detection.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 # Load the exported TensorRT model
-trt_model = YOLO("best-nv2-62.engine")
+trt_model = YOLO("best-rhoban-v1.engine")
 
 # Open the video capture (0 is usually the default camera)
 cap = cv2.VideoCapture(0)
